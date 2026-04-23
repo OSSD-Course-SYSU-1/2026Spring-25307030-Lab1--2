f3685508b475ede56149902a1f705febbf5daed9
dyy-2 <19847269696@163.com>
2026-04-23 22:42:39 +0800

    modify the README.md, and add the sorting.py file

---
+++ sorting.py
@@ -0,0 +1,18 @@
+﻿def bubble_sort(arr):
+    """使用冒泡排序对列表进行原地排序，并返回排序结果。"""
+    n = len(arr)
+    for i in range(n - 1):
+        swapped = False
+        for j in range(n - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+    return arr
+
+
+if __name__ == '__main__':
+    data = [5, 2, 9, 1, 5, 6]
+    print('原始数组:', data)
+    print('排序后:', bubble_sort(data.copy()))
